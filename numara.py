bd5779e86888d04a433902e810f4be6048d096c3
namidegersars <135267099+namidegersars@users.noreply.github.com>
2023-07-21 01:39:05 +0400

    Add files via upload

--- numara.py
+++ numara.py
@@ -1,10 +1,9 @@
 import phonenumbers
 from phonenumbers import geocoder,carrier,timezone
-numara = input("Enter number : ")
+numara = input("Numarayı gir : ")
 telefon = phonenumbers.parse(numara)
-print(f"Location : {geocoder.description_for_number(telefon,'en')}")
-print(f"Carrier : {carrier.name_for_number(telefon,'en')}")
-print(f"Time zone : {timezone.time_zones_for_number(telefon)}")
+print(f"Mekan : {geocoder.description_for_number(telefon,'en')}")
+print(f"Telefon : {carrier.name_for_number(telefon,'en')}")
+print(f"Zaman dilimi : {timezone.time_zones_for_number(telefon)}")
 print(f"Geçerlilik durumu : {phonenumbers.is_valid_number(telefon)}")
 print(f"Format : {phonenumbers.format_number(telefon,phonenumbers.PhoneNumberFormat.NATIONAL)}")
-print(f"")
