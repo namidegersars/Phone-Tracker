8708af02884141fe578f3286185e6ee02242bcd0
namidegersars <135267099+namidegersars@users.noreply.github.com>
2023-07-21 22:50:25 +0400

    Delete numara.py

--- numara.py
+++
@@ -1,9 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder,carrier,timezone
-numara = input("Numarayı gir : ")
-telefon = phonenumbers.parse(numara)
-print(f"Mekan : {geocoder.description_for_number(telefon,'en')}")
-print(f"Telefon : {carrier.name_for_number(telefon,'en')}")
-print(f"Zaman dilimi : {timezone.time_zones_for_number(telefon)}")
-print(f"Geçerlilik durumu : {phonenumbers.is_valid_number(telefon)}")
-print(f"Format : {phonenumbers.format_number(telefon,phonenumbers.PhoneNumberFormat.NATIONAL)}")
